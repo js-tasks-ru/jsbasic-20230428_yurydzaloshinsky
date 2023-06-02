bf07c93ba5135061468f7781f91148e9c5d74cfd
yurykost <yurydzaloshinsky@gmail.com>
2023-06-02 10:23:17 +0300

    решил задачи из 4-module, 5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  const buttonLeft = document.querySelector(".carousel__arrow_left");
+  const buttonRight = document.querySelector(".carousel__arrow_right");
+  const inner = document.querySelector('.carousel__inner');
+  const innerWidth = inner.offsetWidth;
+  const sliderLength = document.querySelectorAll('.carousel__slide').length;
+  
+  let slideCounter = 0;
+
+  currentPosition(slideCounter, buttonRight, buttonLeft, sliderLength);
+     
+  buttonLeft.addEventListener( "click", () => {
+    slideCounter--;
+    inner.style.transform = `translateX(-${slideCounter * innerWidth}px)`;
+    currentPosition(slideCounter, buttonRight, buttonLeft, sliderLength);
+  });
+  
+  buttonRight.addEventListener( "click", () => {
+    slideCounter++;
+    inner.style.transform = `translateX(-${slideCounter * innerWidth}px)`;
+    currentPosition(slideCounter, buttonRight, buttonLeft, sliderLength);
+  });
+   
 }
+
+function currentPosition (slideCounter, buttonRight, buttonLeft, sliderLength) {
+  if (slideCounter === 0) {
+    buttonLeft.style.display = 'none';
+  } else {
+    buttonLeft.style.display = '';
+  }
+  
+  if (slideCounter >= sliderLength -1) {
+    buttonRight.style.display = 'none';
+  } else {
+    buttonRight.style.display = '';
+  }
+  
+}
+
